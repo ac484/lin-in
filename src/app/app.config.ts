93475901b3ac561e4292144b642b6d148e63e826
𓂺 <ac7x7x@gmail.com>
2025-07-10 07:43:08 +0800

    更新專案配置，將專案名稱從 'friendlychat' 更改為 'my-angular-app'，並刪除不再使用的 Firebase 配置文件及相關資源，以簡化應用結構並提升可維護性。

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,24 +1,12 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
-import { AngularFireModule } from '@angular/fire/compat';
-import { AngularFireAuthModule } from '@angular/fire/compat/auth';
-import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
-import { AngularFireFunctionsModule } from '@angular/fire/compat/functions';
-import { AngularFireStorageModule } from '@angular/fire/compat/storage';
-import { AngularFireMessagingModule } from '@angular/fire/compat/messaging';
-import { environment } from '../environments/environment';
-import { routes } from './app.routes';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    importProvidersFrom(
-      AngularFireModule.initializeApp(environment.firebase),
-      AngularFirestoreModule,
-      AngularFireAuthModule,
-      AngularFireFunctionsModule,
-      AngularFireStorageModule,
-      AngularFireMessagingModule
-    ),
+    provideBrowserGlobalErrorListeners(),
+    provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes)
-  ],
+  ]
 };
