4e8a0f2851d9527ff3c77254a2097115ec0acff8
𓂺 <ac7x7x@gmail.com>
2025-07-10 07:30:43 +0800

    新增 Firebase 環境配置，更新 .cursorignore 檔案以包含 Firebase 相關文件，並設置生產環境標誌以提升應用的部署準備性。

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,13 @@
 export const environment = {
+  production: true,
   firebase: {
-    apiKey: 'API_KEY',
-    authDomain: 'PROJECT_ID.firebaseapp.com',
-    databaseURL: 'https://PROJECT_ID.firebaseio.com',
-    projectId: 'PROJECT_ID',
-    storageBucket: 'PROJECT_ID.appspot.com',
-    messagingSenderId: 'SENDER_ID',
-    appId: 'APP_ID',
-    measurementId: 'G-MEASUREMENT_ID',
-  },
-};
+    apiKey: "AIzaSyB9aRKduRC2PXUtEcD2SH2L-1jjIeUix-M",
+    authDomain: "angular-lin-llc.firebaseapp.com",
+    projectId: "angular-lin-llc",
+    storageBucket: "angular-lin-llc.firebasestorage.app",
+    messagingSenderId: "590581941286",
+    appId: "1:590581941286:web:e8509cb6b92c40704652e7",
+    measurementId: "G-CX9CWFLT78",
+    recaptchaV3SiteKey: '6LccRXcrAAAAAI4E9z0NZcujBZ9EnyDJZLLlg9Fp'
+  }
+}; 
