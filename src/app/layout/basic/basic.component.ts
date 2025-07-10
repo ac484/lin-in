6c5d2f946e906ba0d91291842695d7dcd92b7a84
𓂺 <ac7x7x@gmail.com>
2025-07-10 09:53:07 +0800

    簡化基本佈局元件模板，移除不必要的樣式，提升佈局的可讀性與維護性。

--- src/app/layout/basic/basic.component.ts
+++ src/app/layout/basic/basic.component.ts
@@ -14,15 +14,11 @@ import { SidenavComponent } from '../../shared/components/sidenav/sidenav.compon
     SidenavComponent
   ],
   template: `
-    <mat-toolbar color="primary" style="position:fixed;top:0;left:64px;right:0;z-index:1000;">
-      <span style="flex:1 1 auto;"></span>
-      <app-user-avatar />
-    </mat-toolbar>
     <div style="display:flex;min-height:100vh;">
-      <aside style="width:64px;background:#f5f5f5;box-shadow:1px 0 4px #0001;display:flex;flex-direction:column;align-items:center;position:fixed;top:0;left:0;bottom:0;">
+      <aside>
         <app-sidenav />
       </aside>
-      <main style="flex:1;margin-left:64px;padding-top:64px;">
+      <main>
         <router-outlet />
       </main>
     </div>
