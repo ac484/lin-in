9b7c6b9a9c6745adcc892188c3e9fbc9ad88da51
𓂺 <ac7x7x@gmail.com>
2025-07-10 10:35:22 +0800

    整合 UserAvatarComponent 至基本佈局元件，更新模板以提升用戶界面的一致性與可用性。

--- src/app/layout/basic/basic.component.ts
+++ src/app/layout/basic/basic.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { UserAvatarComponent } from '../../shared/components/user-avatar/user-avatar.component';
 import { SidenavComponent } from '../../shared/components/sidenav/sidenav.component';
 
 @Component({
@@ -9,14 +10,19 @@ import { SidenavComponent } from '../../shared/components/sidenav/sidenav.compon
   imports: [
     RouterOutlet,
     MatToolbarModule,
+    UserAvatarComponent,
     SidenavComponent
   ],
   template: `
+    <mat-toolbar color="primary" style="position:fixed;top:0;left:64px;right:0;z-index:1000;">
+      <span style="flex:1 1 auto;"></span>
+      <app-user-avatar />
+    </mat-toolbar>
     <div style="display:flex;min-height:100vh;">
-      <aside>
+      <aside style="width:64px;background:#f5f5f5;box-shadow:1px 0 4px #0001;display:flex;flex-direction:column;align-items:center;position:fixed;top:0;left:0;bottom:0;">
         <app-sidenav />
       </aside>
-      <main>
+      <main style="flex:1;margin-left:64px;padding-top:64px;">
         <router-outlet />
       </main>
     </div>
