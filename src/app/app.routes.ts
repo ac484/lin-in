b1bf88d77f322783144c12d97d5765970b7afc97
𓂺 <ac7x7x@gmail.com>
2025-07-11 23:19:17 +0800

    新增工作區元件路由，更新路由設定以提升用戶導航體驗。

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { PassportComponent } from './shared/components/passport/passport.component';
 import { RegisterComponent } from './shared/components/register/register.component';
 import { ContractComponent } from './components/contract/contract.component';
+import { WorkspaceComponent } from './components/workspace/workspace.component';
 import { authGuard } from './shared/services/auth/auth.guard';
 
 export const routes: Routes = [
@@ -10,6 +11,6 @@ export const routes: Routes = [
   { path: 'passport', component: PassportComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'contract', component: ContractComponent, canActivate: [authGuard] },
-  { path: 'workspace', component: ContractComponent, canActivate: [authGuard] }
+  { path: 'workspace', component: WorkspaceComponent, canActivate: [authGuard] }
 
 ];
